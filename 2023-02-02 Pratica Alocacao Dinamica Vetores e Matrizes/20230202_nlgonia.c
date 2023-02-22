96f319bea661e2aac9988d754584da7158c91405
murilofontes1 <murilofontes1@gmail.com>
2023-02-22 12:18:20 -0300

    added 20230202_tipos_estruturados.c

--- 2023-02-02 Pratica Alocacao Dinamica Vetores e Matrizes/20230202_nlgonia.c
+++ 2023-02-02 Pratica Alocacao Dinamica Vetores e Matrizes/20230202_nlgonia.c
@@ -3,6 +3,7 @@
 
 int main(void){
     int m, n;
+    int qtdCostas = 0;
     printf("Digite o numero de linhas e colunas da matriz:");
     scanf("%d %d", &m, &n);
     if(m < 1 || n > 1000){
@@ -35,24 +36,35 @@ int main(void){
             printf("\n");
         }
 
-        int qtdCostas = 0;
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(matriz[i][j] == '#'){
-                    if(matriz[i][j-1] == '.' || matriz[i][j+1] == '.' || matriz[i-1][j] == '.' || matriz[i+1][j] == '.'){
-                        qtdCostas++;
+                    if(i == 0){
+                        if(matriz[i][j+1] == '.' || matriz[i+1][j] == '.'){
+                            qtdCostas++;
+                        }
+                    }
+
+                    else if(i == m-1){
+                        if(matriz[i][j+1] == '.' || matriz[i-1][j] == '.'){
+                            qtdCostas++;
+                        }
+                    }
+
+                    else{
+                        if(matriz[i][j-1] == '.' || matriz[i][j+1] == '.' || matriz[i-1][j] == '.' || matriz[i+1][j] == '.'){
+                            qtdCostas++;
+                        }
                     }
                 }
-                
-            }
-        }        
-
-        printf("%d", qtdCostas);
-        for(int i = 0; i < m; i++){
-        free(matriz[i]);
+            }                
         }
-    free(matriz);
+        printf("%d", qtdCostas);
+            for(int i = 0; i < m; i++){
+                free(matriz[i]);
+            }
+        free(matriz);
     }
-    
     return 0;
 }
